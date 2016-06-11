1b4272b93617b0270ab5a032f42dc7fa8003e40c
Egorxl <egor.no4@gmail.com>
2016-06-11 16:48:51 +0300

    nothing

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
+import javax.swing.JComponent;
 
-public class Main {
+public class Main extends JComponent  {
+	public static void main(String[] args){
+		windowInit();
+	}
 
+	private static void windowInit() {
+	}
+}
+class MyRunnableThreads implements Runnable{
+
+	@Override
+	public void run() {
+		
+		
+	}
+	
 }
