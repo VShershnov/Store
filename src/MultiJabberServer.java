ad23471ad91839ee23fa1db6dcf6ad07324e2d95
Egorxl <egor.no4@gmail.com>
2016-06-12 13:04:35 +0300

    added threaded network

---
+++ src/MultiJabberServer.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+
+public class MultiJabberServer {
+
+	static final int PORT = 8081;
+
+	public static void main(String[] args) throws IOException {
+		ServerSocket s = new ServerSocket(PORT);
+		System.out.println("Сервер запущено.");
+
+		try {
+			while (true) {
+				Socket socket = s.accept();
+				try {
+					new ServeOneJabber(socket);
+				} catch (IOException e) {
+					// в разі невдачі закриваємо сокет
+					socket.close();
+				}
+			}
+		} finally {
+			s.close();
+		}
+	}
+}
