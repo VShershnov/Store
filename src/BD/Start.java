3fc4b279271687d29a18a85a1ba03eb5d2bde8ac
VShershnov <vshershnyov@gmail.com>
2016-06-28 18:55:22 +0300

    add Anton Base.db

---
+++ src/BD/Start.java
@@ -0,0 +1,20 @@
+package BD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Start {
+
+	public static void main(String[] args) throws ClassNotFoundException,
+			SQLException {
+		// OutputTable.out();
+		//AddIntoTable.addInfo();
+		//DeleteRow.delete();
+		//OutputTable.out();
+		//SearchForName.search();
+		EditUpdeteName.edit();
+	}
+}
