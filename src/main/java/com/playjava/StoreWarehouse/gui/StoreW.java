669e9efc42fce7202539f67db96d82d5193506f5
Vitalii Shershnyov <vshershnyov@gmail.com>
2017-10-27 12:45:34 +0300

    change package structure

---
+++ src/main/java/com/playjava/StoreWarehouse/gui/StoreW.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.playjava.StoreWarehouse.gui;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author user
+ */
+public class StoreW {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.ClassNotFoundException
+     * @throws java.sql.SQLException
+     */
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        StoreWForm form = new StoreWForm();      
+        
+        form.setVisible(true);
+    }
+    
+}
